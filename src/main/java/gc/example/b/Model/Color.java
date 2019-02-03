96a37b1e2b8745b3f6d3d303df18f1319593c52a
Iñigo <Iigo>
2019-02-02 21:58:49 -0500

    Getter and Setter

--- src/main/java/gc/example/b/Model/Color.java
+++ src/main/java/gc/example/b/Model/Color.java
@@ -2,12 +2,27 @@ package gc.example.b.Model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class Color{
     private String color;
 
     public Color() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     private String description;
